adbcbf4a5f2ff7e0489958db5db1c17ed44b7a05
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2020-04-11 14:22:31 -0500

    ADD(): CONFIG

---
+++ src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/another">Another Page</router-link>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/AnotherPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      <h1>Hello form another Page</h1>
+    </div>
+</template>
