0c6c3ce4966794adde7a1864382182eafdc2baca
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2020-05-26 02:22:15 -0500

    FIX(): UI ISSUES

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,36 +1,36 @@
 <template>
     <div class="flex items-center justify-center h-screen p-8  bg-sa-yellow" :class="{'flex-col text-lg justify-between': $mq === 'mo', 'text-lg': $mq === 'de'}">
-        <div class="w-1/3 flex flex-col bg-black text-left  rounded-lg box-shadow" :class="{'flex-col w-full m-0  p-2': $mq === 'mo' , 'mr-16 p-8': $mq === 'de'}">
-            <p class="flex items-center pb-4  text-sa-yellow font-bold" :class="{'text-2xl': $mq === 'de'}" >Registro de visitantes <span class="material-icons px-4">
+        <div class="w-1/3 flex flex-col bg-black text-left  rounded-lg box-shadow" :class="{'flex-col w-full m-0  p-2': $mq === 'mo' , 'mr-8 p-8': $mq === 'de'}">
+            <p class="flex items-center pb-4  text-sa-yellow font-bold" :class="{'text-2xl': $mq === 'de'}" >Terminal<span class="material-icons px-4">
                recent_actors
                 </span>
             </p>
             <img v-if="$mq==='de'" src="../assets/terminal1.png" alt="">
-            <p v-if="$mq==='de'" class="text-secondary pt-8 text-center">Registro de vistantes del co-working "Terminal 1". Desarrollado con vanilla JS, Cloud Firestore and Formspree.</p>
+            <p v-if="$mq==='de'" class="text-secondary pt-8 text-center box">Registro de vistantes. Desarrollado con vanilla JS, Cloud Firestore and Formspree.</p>
             <div class="flex text-primary pt-8 justify-center">
                 <BioButton href="https://github.com/DianaCarrillo/cdmx-2018-06-am-bc-registro-de-visitantes" label="CODE" />
                 <BioButton href="https://dianacarrillo.github.io/cdmx-2018-06-am-bc-registro-de-visitantes/src/" label="DEMO" />
             </div>
         </div>
-        <div class="w-1/3 flex flex-col  text-left bg-black rounded-lg box-shadow" :class="{'flex-col w-full p-2': $mq === 'mo' , 'mx-16 p-8': $mq === 'de'}">
+        <div class="w-1/3 flex flex-col  text-left bg-black rounded-lg box-shadow" :class="{'flex-col w-full p-2': $mq === 'mo' , 'mx-8 p-8': $mq === 'de'}">
             <p class="flex items-center pb-4 text-sa-yellow font-bold" :class="{'text-2xl': $mq === 'de'}">Greener <span class="material-icons px-4">
                 face
                 </span>
             </p>
             <img v-if="$mq==='de'" src="../assets/greener.png" alt="">
-            <p v-if="$mq==='de'" class="text-secondary pt-8 text-center">Red social sobre nutrición y salud "Greener". Desarrollada con vanilla JS y Firebase.</p>
+            <p v-if="$mq==='de'" class="text-secondary pt-8 text-center box">Red social sobre nutrición y salud "Greener". Desarrollada con vanilla JS y Firebase.</p>
              <div class="flex text-primary pt-8 justify-center">
                 <BioButton href="https://github.com/DianaCarrillo/cdmx-social-network-frameworks" label="CODE" />
                 <BioButton href="https://dianacarrillo.github.io/cdmx-social-network-frameworks/" label="DEMO" />
             </div>
         </div>
-                <div class="w-1/3 flex flex-col  text-left bg-black rounded-lg box-shadow" :class="{'flex-col w-full p-2': $mq === 'mo' , 'ml-16 p-8': $mq === 'de'}">
+                <div class="w-1/3 flex flex-col  text-left bg-black rounded-lg box-shadow" :class="{'flex-col w-full p-2': $mq === 'mo' , 'ml-8 p-8': $mq === 'de'}">
             <p class="flex items-center pb-4 text-sa-yellow font-bold" :class="{'text-2xl': $mq === 'de'}">Foodmap  <span class="material-icons px-4">
                 place
                 </span>
             </p >
             <img v-if="$mq==='de'" src="../assets/foodmap.png" alt="">
-            <p v-if="$mq==='de'" class="text-secondary pt-8 text-center"> Buscador de restaurantes consumiendo Google Maps API. Desarrollado con with vanilla JS.</p>
+            <p v-if="$mq==='de'" class="text-secondary pt-8 text-center box"> Buscador de restaurantes consumiendo Google Maps API. Desarrollado con with vanilla JS.</p>
             <div class="flex text-primary pt-8 justify-center">
                 <BioButton href="https://github.com/DianaCarrillo/cdmx-2018-06-bc-core-pm-foodmap" label="DEMO" />
                 <BioButton href="https://dianacarrillo.github.io/cdmx-2018-06-bc-core-pm-foodmap/src/views/view1.html" label="CODE" />
@@ -56,6 +56,9 @@ import BioButton from '../components/BioButton'
 </script>
 
 <style lang="scss" scoped>
+.box{
+    height: 100px
+}
 // .bg-color{
 //       background: rgba(25, 25, 25, .5);
 // }
